4e5854d498324f030fd31d7ff24519117e9bf8b5
adrian92612 <adrian.villamin0612@gmail.com>
2024-09-09 19:48:51 +0800

    added shadcn form add home page login link

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main className="h-screen w-full flex items-center justify-center">
+      <div>
+        <h1>Rental Property App</h1>
+        <Button asChild>
+          <Link href="/auth/login">Login</Link>
+        </Button>
+      </div>
+    </main>
+  );
 }
