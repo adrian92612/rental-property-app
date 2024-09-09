56cfc2e772b6d5e6784c9e2c55f3f2989eac49d1
adrian92612 <adrian.villamin0612@gmail.com>
2024-09-10 00:19:00 +0800

    added login social form

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,14 +1,16 @@
 import { LoginForm } from "@/components/auth/login-form";
+import { LoginSocialForm } from "@/components/auth/login-social-form";
 
 const LoginPage = () => {
   return (
-    <div className="w-11/12 max-w-[300px]">
+    <div className="w-11/12 max-w-[300px] space-y-2">
       <LoginForm />
       <div className="flex justify-center items-center gap-2">
         <div className="flex-grow bg-gray-200 h-px"></div>
         <span>or</span>
         <div className="flex-grow bg-gray-200 h-px"></div>
       </div>
+      <LoginSocialForm />
     </div>
   );
 };
---
+++ components/auth/login-social-form.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useActionState } from "react";
+import { Button } from "../ui/button";
+import { FaGithub, FaGoogle } from "react-icons/fa";
+
+export const LoginSocialForm = () => {
+  // const [state,action,pending] = useActionState()
+  return (
+    <form action="" className="flex items-center gap-2">
+      <Button
+        variant="outline"
+        size="sm"
+        name="action"
+        value="google"
+        className="flex-grow border-primary"
+      >
+        <FaGoogle className="h-5 w-5" />
+      </Button>
+      <Button
+        variant="outline"
+        size="sm"
+        name="action"
+        value="github"
+        className="flex-grow border-primary"
+      >
+        <FaGithub className="h-5 w-5" />
+      </Button>
+    </form>
+  );
+};
