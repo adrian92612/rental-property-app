974b796f9c966470e8a33d8e1fc30ae6ea46b5ac
adrian92612 <adrian.villamin0612@gmail.com>
2024-09-10 14:47:58 +0800

    added auth js

---
+++ auth.ts
@@ -0,0 +1,74 @@
+import NextAuth from "next-auth";
+import prisma from "@/prisma/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { AdapterUser } from "next-auth/adapters";
+import { createId } from "@paralleldrive/cuid2";
+import Google from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  pages: {
+    signIn: "/auth/login",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.AUTH_SECRET,
+  adapter: {
+    ...PrismaAdapter(prisma),
+    createUser: async (data: any) => {
+      data.id = createId();
+      return await prisma.user.create({ data });
+    },
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const pathname = nextUrl.pathname;
+
+      const redirectIfLoggedIn = ["/", "/auth/login", "/auth/register"];
+      const isOnProtectedPage = pathname.startsWith("/dashboard");
+
+      if (isLoggedIn && redirectIfLoggedIn.includes(pathname)) {
+        return Response.redirect(new URL("/dashboard", nextUrl));
+      }
+      if (!isLoggedIn && isOnProtectedPage) {
+        return Response.redirect(new URL("/auth/login", nextUrl));
+      }
+
+      return true;
+    },
+    async jwt({ token, user }) {
+      if (user) token.id = user.id;
+      return { id: token.id };
+    },
+    async session({ session, token }) {
+      // session.user.id = token.id as string;
+      session.user = { id: token.id as string } as AdapterUser & { id: string };
+      return session;
+    },
+  },
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      profile(profile) {
+        return {
+          id: profile.sub,
+          firstName: profile.given_name,
+          lastName: profile.family_name,
+          email: profile.email,
+          image: profile.picture,
+          emailVerified: profile.email_verified,
+        };
+      },
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+});
