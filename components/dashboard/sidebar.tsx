17c9bf8151969bc30836310f5eaedd995abbbcbe
adrian92612 <adrian.villamin0612@gmail.com>
2024-09-10 16:52:09 +0800

    added sidebar

---
+++ components/dashboard/sidebar.tsx
@@ -0,0 +1,98 @@
+import { logout } from "@/lib/actions/actions";
+import { Button } from "../ui/button";
+import { Popover, PopoverContent } from "../ui/popover";
+import { IoIosMenu } from "react-icons/io";
+import { PopoverTrigger } from "@radix-ui/react-popover";
+import { BsBuildingsFill, BsPeopleFill } from "react-icons/bs";
+import { FaKey } from "react-icons/fa6";
+import { MdSpaceDashboard } from "react-icons/md";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { auth } from "@/auth";
+
+const links = [
+  {
+    href: "/dashboard",
+    label: "Overview",
+    icon: <MdSpaceDashboard />,
+  },
+  {
+    href: "/dashboard/properties",
+    label: "Properties",
+    icon: <BsBuildingsFill />,
+  },
+  {
+    href: "/dashboard/units",
+    label: "Units",
+    icon: <FaKey />,
+  },
+  {
+    href: "/dashboard/tenants",
+    label: "Tenants",
+    icon: <BsPeopleFill />,
+  },
+];
+
+const NavLinks = () => {
+  return (
+    <nav>
+      <ul>
+        {links.map((link) => (
+          <li key={link.label}>
+            <Button
+              asChild
+              variant="link"
+              size="lg"
+              className="text-primary-foreground text-xl hover:bg-primary-foreground hover:text-primary w-full justify-start rounded-none"
+            >
+              <Link href={link.href} className="flex items-center gap-3">
+                {link.icon} {link.label}
+              </Link>
+            </Button>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+const LogoutBtn = () => {
+  return (
+    <form
+      action={logout}
+      className="flex items-center gap-2 sm:border-t border-primary-foreground sm:pt-2 w-full justify-end"
+    >
+      <Avatar>
+        <AvatarImage src="" alt="Picture of user" />
+        <AvatarFallback>AV</AvatarFallback>
+      </Avatar>
+      <Button variant="destructive" size="sm" className="font-bold rounded-sm">
+        Logout
+      </Button>
+    </form>
+  );
+};
+
+export const SideBar = async () => {
+  // const user = await auth();
+  // console.log(user);
+  return (
+    <div className="sm:sticky sm:top-0 sm:h-screen flex sm:flex-col items-center justify-between px-5 py-3 sm:py-5 bg-primary">
+      <div className="sm:hidden">
+        <Popover>
+          <PopoverTrigger>
+            <IoIosMenu className="h-8 w-8 text-primary-foreground" />
+          </PopoverTrigger>
+          <PopoverContent className="bg-primary">
+            <NavLinks />
+          </PopoverContent>
+        </Popover>
+      </div>
+
+      <div className="hidden sm:block">
+        <NavLinks />
+      </div>
+      <LogoutBtn />
+    </div>
+  );
+};
