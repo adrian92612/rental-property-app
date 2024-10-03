a0df58eeffee47c9f9ad8cec29c9dda4f78fbec3
adrian92612 <adrianvillamin0612@gmail.com>
2024-10-03 20:01:25 +0800

    added some styling on homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,32 @@
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import Link from "next/link";
+import { BsFillBuildingsFill } from "react-icons/bs";
 
 export default function Home() {
   return (
     <main className="h-screen w-full flex items-center justify-center bg-gradient-to-r from-slate-500 to-slate-800">
-      <div>
-        <h1>Rental Property App</h1>
-        <Button asChild>
-          <Link href="/auth/login">Login</Link>
-        </Button>
-      </div>
+      <Card className="bg-transparent border-none">
+        <CardHeader>
+          <CardTitle className="font-bold text-5xl text-primary-foreground flex flex-col items-center gap-2">
+            <BsFillBuildingsFill /> Rental Property Dashboard
+          </CardTitle>
+          <CardDescription className="text-muted">
+            Manage your properties with ease
+          </CardDescription>
+        </CardHeader>
+        <CardFooter className="justify-end pb-5">
+          <Button variant="secondary" className="font-bold" asChild>
+            <Link href="/auth/login">Login</Link>
+          </Button>
+        </CardFooter>
+      </Card>
     </main>
   );
 }
