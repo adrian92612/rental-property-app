b795432fe4d1ab7dade0a06f8a97e3417858ab70
adrian92612 <adrianvillamin0612@gmail.com>
2024-10-08 23:57:04 +0800

    added login validation test. added dashboard navigation test.

--- cypress/e2e/4-dashboard-navigations.cy.ts
+++ cypress/e2e/4-dashboard-navigations.cy.ts
@@ -1,6 +1,8 @@
 import "cypress-real-events";
 
 describe("dashboard navigations", () => {
+  const baseUrl = Cypress.config("baseUrl");
+
   beforeEach(() => {
     cy.visit("/auth/login");
     cy.get('input[name="email"]').type("cypress@test.com");
@@ -17,18 +19,21 @@ describe("dashboard navigations", () => {
     cy.get("a")
       .contains(/properties/i)
       .click();
+    cy.url().should("eq", `${baseUrl}/dashboard/properties`);
     cy.get("h1")
       .contains(/properties/i)
       .should("be.visible");
 
     // units
     cy.get("a").contains(/units/i).click();
+    cy.url().should("eq", `${baseUrl}/dashboard/units`);
     cy.get("h1").contains(/units/i).should("be.visible");
 
     // tenants
     cy.get("a")
       .contains(/tenants/i)
       .click();
+    cy.url().should("eq", `${baseUrl}/dashboard/tenants`);
     cy.get("h1")
       .contains(/tenants/i)
       .should("be.visible");
@@ -37,6 +42,7 @@ describe("dashboard navigations", () => {
     cy.get("a")
       .contains(/settings/i)
       .click();
+    cy.url().should("eq", `${baseUrl}/dashboard/user-settings`);
     cy.get("h1")
       .contains(/user settings/i)
       .should("be.visible");
